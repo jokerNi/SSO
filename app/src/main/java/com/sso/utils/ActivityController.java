b19a7755d4df40ec6d3b8657c4dcea177c65ffce
王立强 <wangliqiangorz@163.com>
2016-07-29 15:32:24 +0800

    add

---
+++ app/src/main/java/com/sso/utils/ActivityController.java
@@ -0,0 +1,38 @@
+package com.sso.utils;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 王立强
+ * @Date 2016/7/29.
+ */
+public class ActivityController {
+    public static List<Activity> activities=new ArrayList<Activity>();
+
+    public static void addActivity(Activity activity){
+
+        activities.add(activity);
+
+    }
+
+    public static void removeActivity(Activity activity){
+
+        activities.remove(activity);
+
+    }
+
+    public static void finishAll(){
+
+        for(Activity activity:activities){
+
+            if(activity.isFinishing()){
+
+                activity.finish();
+            }
+        }
+
+    }
+}
